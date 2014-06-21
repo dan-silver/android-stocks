76f8003c0bbfb45da76a201b8579fe9b4f264539
Dan Silver <das2c3@mail.missouri.edu>
2014-06-21 12:53:45 -0500

    Formats x-axis as timestamps

--- app/src/main/java/dan/stocks/StockDetailFragment.java
+++ app/src/main/java/dan/stocks/StockDetailFragment.java
@@ -3,6 +3,7 @@ package dan.stocks;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,7 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.jjoe64.graphview.CustomLabelFormatter;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.GraphViewSeries;
 import com.jjoe64.graphview.GraphView.GraphViewData;
@@ -22,6 +24,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.HashMap;
 
 /**
@@ -47,7 +51,6 @@ public class StockDetailFragment extends Fragment {
         if (args != null) {
             // Set article based on argument passed in
             updateArticleView(args.getInt(STOCK_DB_ID));
-
         }
 
         Button removeStock = (Button) getActivity().findViewById(R.id.remove_stock);
@@ -82,16 +85,22 @@ public class StockDetailFragment extends Fragment {
                 GraphViewData[] data;
                 try {
                     JSONArray res = new JSONArray(response);
+                    DateFormat dateTimeFormatter = DateFormat.getDateTimeInstance();
                     data = new GraphViewData[res.length()];
                     for (int i = 0; i < res.length(); i++) {
                         JSONObject o = res.getJSONObject(i);
-//                        data[i] = new GraphViewData(o.getLong("datetime"), o.getDouble("value"));
-                        data[i] = new GraphViewData(i, o.getDouble("value"));
+                        data[i] = new GraphViewData(o.getLong("datetime") * 1000, o.getDouble("value"));
+                        //data[i] = new GraphViewData(1403367785894l + i * 10000l, i*10);
+                        //data[i] = new GraphViewData(1403367785894l + i * 10000l, i*10);
+                        Log.v("a1a1", dateTimeFormatter.format(new Date((long) data[i].getX())));
                     }
+
                 } catch (JSONException e) {
                     e.printStackTrace();
                     data = null;
                 }
+
+
                 updateGraph(data);
             }
         });
@@ -109,11 +118,20 @@ public class StockDetailFragment extends Fragment {
         if (layout != null) {
             GraphView graphView = new LineGraphView(getActivity().getApplicationContext(), "");
             graphView.addSeries(new GraphViewSeries(data));
-            graphView.setViewPort(2, 40); // set view port, start=2, size=40
-
-            // optional - activate scaling / zooming
+//            graphView.setViewPort(2, 40); // set view port, start=2, size=40
+//
+//            // optional - activate scaling / zooming
             graphView.setScrollable(true);
             graphView.setScalable(true);
+            final android.text.format.DateFormat df = new android.text.format.DateFormat();
+            graphView.setCustomLabelFormatter(new CustomLabelFormatter() {
+                @Override
+                public String formatLabel(double value, boolean isValueX) {
+                    if (isValueX)
+                        return df.format("MM/dd/yyyy", new Date((long) value)).toString();
+                    return null; // let graphview generate Y-axis label for us
+                }
+            });
             layout.addView(graphView);
         }
     }
