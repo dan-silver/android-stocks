e1d898e939316835fe421a1b1d91b3b944f22bc0
Dan Silver <das2c3@mail.missouri.edu>
2014-06-20 19:42:17 -0500

    Extra semicolon

--- app/src/main/java/dan/stocks/StockDetailFragment.java
+++ app/src/main/java/dan/stocks/StockDetailFragment.java
@@ -33,7 +33,7 @@ public class StockDetailFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.stock_detail, container, false);;
+        return inflater.inflate(R.layout.stock_detail, container, false);
     }
 
     @Override
